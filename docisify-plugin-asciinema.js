4b2ffd3edd902eb94a7405932f7be506d2367c04
Marcelo Arroyo <marcelo.arroyo@gmail.com>
2023-04-21 16:21:04 -0300

    Create docisify-plugin-asciinema.js

---
+++ docisify-plugin-asciinema.js
@@ -0,0 +1,28 @@
+(function () {
+    var asciinemaPlugin = function (hook, vm) {
+      
+      // Invoked on each page load after new HTML has been appended to the DOM
+      hook.doneEach(function () {
+        const asciinemaElements = document.getElementsByClassName('asciinema');
+        for (let e of asciinemaElements) {
+            AsciinemaPlayer.create(e.dataset.value, e);
+        }
+      });
+
+    };
+  
+    // Add plugin to docsify's plugin array
+    $docsify = $docsify || {};
+    $docsify.plugins = [].concat(asciinemaPlugin, $docsify.plugins || []);
+
+    // add docsify markdown image renderer to generate an asciinema element
+    $docsify.markdown.renderer.image = function(href, title, text) {
+      if (href.endsWith('.cast')) {
+          // generate div for asciinema player
+          const attrs = `id=${text} data-value="${href}"`;
+          let html = `<div ${attrs} class="asciinema"></div>\n`;
+          return html + `<p class="caption">${title}</p>\n`;
+      }
+      return false;
+    }
+  })();
